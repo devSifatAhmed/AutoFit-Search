971825b4d77b894ef758cb11d86ca25ce3d8c000
Sifat Ahmed <programmer.sifatahmed@gmail.com>
2026-05-18 18:32:11 +0600

    Note(): Created the empty billing page

---
+++ app/routes/app.billing.jsx
@@ -0,0 +1,13 @@
+export default function Billing() {
+    return (
+        <s-stack gap="large">
+            <s-box>
+                <s-grid columns="1fr" gap="small">
+                    <s-paragraph>
+                        This is the billing page. You can manage your billing information and view your invoices here.
+                    </s-paragraph>
+                </s-grid>
+            </s-box>
+        </s-stack>
+    );
+}
