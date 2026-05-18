b2711c458258810f8f5e4c6ed9c25133bc1dd0f8
Sifat Ahmed <programmer.sifatahmed@gmail.com>
2026-05-18 18:33:25 +0600

    Note(): Billing page added to app nav

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -18,6 +18,7 @@ export default function App() {
       <s-app-nav>
         <s-link href="/app">Dashboard</s-link>
         <s-link href="/app/database">Database</s-link>
+        <s-link href="/app/billing">Billing</s-link>
       </s-app-nav>
       <Outlet />
     </AppProvider>
